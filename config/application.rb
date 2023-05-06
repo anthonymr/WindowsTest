f0719fabbf3e5d92d9f9acdb37ee86e0c4584aa0
Anthony Martin <anthonymartin20@gmail.com>
2023-05-06 10:28:39 -0400

    Change hardcoded text by language files

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module WindowsTest
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.available_locales = [:en, :es]
+    config.i18n.default_locale = :en
   end
 end
